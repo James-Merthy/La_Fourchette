48c7ce30c363dcb2b9b3d4c4c70ca20dfb7ec01d
james <anasala.merthy@gmail.com>
2022-04-27 12:20:40 +0200

    Création de la méthode askNumber

--- src/laFourchette/Game.java
+++ src/laFourchette/Game.java
@@ -1,2 +1,40 @@
-package laFourchette;public class Game {
+package laFourchette;
+
+public class Game {
+
+    /**
+     * Cette méthode retourne un nombre aléatoire
+     * de 1 à 100
+     *
+     * @return
+     */
+    public static int nombreAléatoire() {
+
+        return (int) (Math.random() * 101);
+    }
+
+    /**
+     * Cette méthode demande a l'utilisateur
+     * de taper un chiffre
+     * cette méthode retourne le chiffre 
+     * @return
+     */
+    public static int askNumber(){
+
+        return 0 ;
+    }
+
+    public static void main(String[] args) {
+
+        int i = 0 ;
+
+        while (i < 1000){
+
+            System.out.println(nombreAléatoire());
+
+            i ++ ;
+
+        }
+
+    }
 }
