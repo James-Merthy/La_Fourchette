fec677410579539707c73113ecc6260a329aca43
james <anasala.merthy@gmail.com>
2022-04-27 13:16:36 +0200

    La Game Final

--- src/laFourchette/Game.java
+++ src/laFourchette/Game.java
@@ -78,13 +78,66 @@ public class Game {
 
     }
 
+    /**
+     * Cette méthode vas tout qimplement lancer la game
+     * et dire a l'utilisateur si son nombre est plus grand
+     * ou plus petit que le nombre du bot
+     * il a 5 chances pas une de plus
+     */
+    public static void la_Game (){
+
+        System.out.println("=============== La fourchette ==========================");
+        int nombre_de_chance = 5 ;
+
+
+
+        int Le_bon_nombre = nombreAléatoire();
+
+        while (nombre_de_chance > 0) {
+
+            String displayChance = " | ***** tentative : " + nombre_de_chance + " ***** |";
+
+            String displayToure = "------------------------------------------------------";
+
+            System.out.println(displayToure);
+
+            System.out.println(displayChance);
+
+            int nb = askNumber() ;
+
+            if (comparaison(Le_bon_nombre, nb)){
+                System.out.println("vous avez trouvez le nombre bravo !!!");
+                nombre_de_chance = -1 ;
+
+            } else if (nb > Le_bon_nombre) {
+
+                System.out.println("votre nombre est trop grand");
+
+                nombre_de_chance -- ;
+            }else{
+
+                System.out.println("votre nombre est trop petit");
+                nombre_de_chance --;
+            }
+
+
+
+        }
+
+        if (nombre_de_chance == 0){
+
+            System.out.println("vous avez perdu !");
+        }
+
+
+
+    }
+
     public static void main(String[] args) {
 
         int i = 0;
 
-        System.out.println(comparaison(5,5));
-        System.out.println(comparaison(7,5));
-        System.out.println(comparaison(nombreAléatoire(),askNumber()));
+        la_Game();
 
     }
 }
